05b316fafe6264a520bdbdb9273b482f9946c68f
kth2642 <kth2642@naver.com>
2022-02-17 19:04:18 +0900

    create routers

--- src/server.js
+++ src/server.js
@@ -1,27 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000
 
 // express app 생성 
 const app = express();
 const logger = morgan("dev");
-
-// home directory에 대한 callback 함수 (request, response, 이 함수 다음에 실행할 함수)
-const home = (req, res) => {
-    console.log("I will respond.");
-    return res.send("Hello");
-};
-
-const login = (req, res) => {
-    return res.send("login");
-};
-
 // global middleware 생성 
 app.use(logger);
-// "/"(root)에 대한 get method를 통한 처리 설정("위치", "callback 함수")
-app.get("/", home);
-app.get("/login", login);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 // callback 생성 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
